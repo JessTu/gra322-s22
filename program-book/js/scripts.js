849ca2ab6541a89bb15c0b3d306c9dbdd32f7512
JessTu <jessic10@asu.edu>
2022-04-05 21:53:15 -0700

    styled mobile

---
+++ program-book/js/scripts.js
@@ -0,0 +1,79 @@
+$(document).ready(function(){
+    /*
+    we're finding the nav element and hiding it here
+    */
+    $('nav').hide();
+
+    // setup our click function
+    $('.menu-button').click(function(){
+        $('nav').slideToggle();
+    });
+});
+
+const slider = tns({
+    container: '.my-slider',
+    nav: false,
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    responsive: {
+        375: {
+          edgePadding: 50,
+          gutter: 20,
+          items: 1
+        },
+        768: {
+          gutter: 30
+        },
+        1400: {
+          items: 1
+        }
+      }
+});
+
+
+const slider2 = tns({
+    container: '.my-slider2',
+    nav: false,
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    responsive: {
+        375: {
+          edgePadding: 50,
+          gutter: 20,
+          items: 1
+        },
+        768: {
+          gutter: 30
+        },
+        1400: {
+          items: 1
+        }
+      }
+});
+
+
+const slider3 = tns({
+    container: '.my-slider3',
+    nav: false,
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    responsive: {
+        375: {
+          edgePadding: 50,
+          gutter: 20,
+          items: 1
+        },
+        768: {
+          gutter: 30
+        },
+        1400: {
+          items: 1
+        }
+      }
+});
+
+
+
